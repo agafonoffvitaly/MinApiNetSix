fd256476c820500b02a28c079762152271ec1998
Agafonov <agafonoff.vv@gmail.com>
2022-06-13 00:03:59 +0300

    EF added

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MinApiNetSix.Models;
+
+namespace MinApiNetSix.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Command> Commands => Set<Command>();
+}
